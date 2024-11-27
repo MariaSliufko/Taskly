cf8284498ff66bfa1b4e14c7751a7795ad02a51c
MariaSl <maria.sliufko@icloud.com>
2024-11-27 14:36:17 +0100

    setup: Finalized frontend setup and integrated backend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,14 @@
+import { useEffect } from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>Frontend kopplad till Backend</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
